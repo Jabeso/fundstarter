c5cee5eaadb5e8c8ee0d8fcd3aba289d25a82fad
Michael Daniels <michael.edaniels47@gmail.com>
2016-03-17 20:29:06 -0700

    Part 2

--- index.js
+++ index.js
@@ -14,18 +14,51 @@ console.log("Listening on port: " + port);
 */
 
 /*1B*/
+/*
 var fs = require('fs')
 var http = require('http')
 var port = process.env.port || 8000
 
 var requestListener = function(request, response){
     fs.readFile('index.html' , function(error, content){
+    	if (error){
+    		response.writeHead(500);
+    		return console.log(error);
+    	}else{
     		response.writeHead(200);
     		response.end(content, 'utf8');
-    	
+    	}
     });
 }
 
 var server = http.createServer(requestListener);
 server.listen(port);
 console.log("Listening on port: " + port);
+*/
+var fs = require('fs')
+var http = require('http')
+var fileName = "index.html";
+var port = process.env.port || 8000
+
+var requestListener = function (request, response) { 
+	fs.exists(fileName, function(exists) {
+	  if (exists) {
+	    fs.stat(fileName, function(error, stats) {
+	      fs.open(fileName, "r", function(error, fd) {
+	        var buffer = new Buffer(stats.size);
+
+	        fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
+	          var data = buffer.toString("utf8", 0, buffer.length);
+	          response.writeHead(200, {'Context-Type':'text/html'});
+			  response.end(data,'utf8');
+	          fs.close(fd);
+	        });
+	      });
+	    });
+	  }
+	});
+}
+
+var server = http.createServer(requestListener);
+server.listen(port);
+console.log('Server running on port: ', port)
