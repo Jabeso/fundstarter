06d1534def6a64fbe2b95e413fae874649492b15
Michael Daniels <michael.edaniels47@gmail.com>
2016-03-17 20:05:01 -0700

    1B done

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 /*1A*/
+/*
 var fs = require('fs')
 var http = require('http')
 var port = process.env.port || 8080
 
 var requestListener = function(request, response){
     response.end(fs.readFileSync('index.html'), 'utf8');
+}
+
+var server = http.createServer(requestListener);
+server.listen(port);
+console.log("Listening on port: " + port);
+*/
+
+/*1B*/
+var fs = require('fs')
+var http = require('http')
+var port = process.env.port || 8080
+
+var requestListener = function(request, response){
+    fs.readFile('index.html' , function(error, content){
+    	if (error){
+    		response.writeHead(500);
+    		response.end();
+    	}else{
+    		response.writeHead(200);
+    		response.end(content, 'utf8');
+    	}
+    });
 }
 
 var server = http.createServer(requestListener);
